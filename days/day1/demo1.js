18b664747dce8f3b59c3c3cde8ae2c6aebbf5a4b
sasidhar1786 <sasidhar1786@gmail>
2024-04-18 12:07:12 +0530

    firstcommit

---
+++ days/day1/demo1.js
@@ -0,0 +1,26 @@
+var a= 10;
+var b=20;
+// var c=30;
+var i='2';
+var j= 2;
+ let c= a++;
+ let d = b--;
+
+console.log(`squares the values ${a} is ${a**2}`);
+console.log(`cubes the values ${a} is ${a**3}`);
+console.log(`adding the values ${a}+${b} is ${a+b}`);
+console.log(`sub the values ${a}-${b} is ${a-b}`);
+console.log(`multi the values ${a}*${b} is ${a*b}`);
+console.log(`divis the values ${a}/${b} is ${a/b}`);
+console.log(`percent the values ${b}%${a} is ${b%a}`);
+console.log(`increment the values a ${a} is ${c}`);
+console.log(`decrement the values b ${b} is ${d}`);
+console.log(` the values a ${a}> b${b} is ${a>b}`);
+console.log(` the values a ${a}< b${b} is ${a<b}`);
+console.log(` the values a ${a}<= b${b} is ${a<=b}`);
+console.log(` the values a ${a}>= b${b} is ${a>=b}`);
+console.log(` not the values a !${a}>= b${b} is ${!(a>=b)}`);
+console.log(` and the values a ${a}&& b${b} is ${(a&&b)}`);
+console.log(`  the values a ${a}||b${b} is ${(a||b)}`);
+console.log(`== the values i ${i}== j${j} is ${i==j} `)
+console.log(`=== the values i ${i}=== j${j} is ${i===j} `)
