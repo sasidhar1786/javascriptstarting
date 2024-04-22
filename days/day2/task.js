f70dfb5448e2f0b388be6c0b7b0453b71cd67f1b
sasidhar1786 <sasidhar1786@gmail>
2024-04-22 10:06:30 +0530

    third

---
+++ days/day2/task.js
@@ -0,0 +1,50 @@
+// var a = 'buzz';
+// var b= "fizz";
+
+// for (i=1;i<=15;i++){
+//     if(i%3!==0&&i%5!==0){
+//         console.log(i)
+//     }else if(i%3==0){
+//         console.log(a)
+//     }else if(i%5==0){
+//         console.log(b)
+//     }
+// }
+
+
+// var k = +prompt("enter number");
+
+// (k>10)?console.log("hello friends"):console.log("error");
+
+
+// if(k<=11){
+//     if(k<=11){
+//         console.log("good morning");
+//     }else if(k<=17){
+//         console.log("good afternon");
+//     }else if(k<=24||k<=4){
+//         console.log("good night")
+//     }
+
+// }else{
+//     console.log("enter the present time")
+// }
+
+
+// var char = prompt("enter the time now");
+// if (char === 'a' || char === 'e' || char=== 'i' || char === 'o' || char === 'u') {
+//     console.log(char + " is a vowel.");
+// } else {
+//     console.log(char + " is a consonant.");
+// }
+var l= +prompt("enter time")
+ 
+if (l>=24){
+    // if(l>=10){
+    //     console.log("good morning");
+    // }else if(l>=17){
+        console.log("good moring");
+    // }
+}else if(l>=10){
+    console.log("enter good afternoon");
+}
---
+++ days/day3/task.js
@@ -0,0 +1,5 @@
+var j=0;
+for (i=0;i<=700;i=i+17){
+    debugger;
+    console.log( `17 x ${j++} = ${i}`)
+}
